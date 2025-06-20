a1c12fb42cd948cecd9ff344a942dba1c897a9cd
fvrvz <codefaraaz@gmail.com>
2025-06-20 20:29:31 +0530

    feat: add edit functionality for todo

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,21 +1,52 @@
 <script lang="ts" setup>
 import { useTodoStore } from '@/stores/todo.store'
 import type { Todo } from '@/types/todo.type'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faXmark, faPen, faFloppyDisk, faTrash } from '@fortawesome/free-solid-svg-icons'
 import IconButton from './IconButton.vue'
+import { ref } from 'vue'
 
 const { todo } = defineProps<{ todo: Todo }>()
 const store = useTodoStore()
 function onRemove() {
   store.removeTodo(todo.id)
 }
+
+const isEditMode = ref<boolean>(false)
+const todoTitle = ref<string>(todo.title)
+
+function onSave() {
+  const _todo: Todo = {
+    ...todo,
+    title: todoTitle.value,
+  }
+
+  store.editTodo(todo.id, _todo)
+  todoTitle.value = ''
+  isEditMode.value = false
+}
+
+function toggleEdit() {
+  isEditMode.value = !isEditMode.value
+  todoTitle.value = todo.title
+}
 </script>
 
 <template>
   <article class="todo-container">
     <input type="checkbox" :checked="todo.isCompleted" @change="store.toggleTodo(todo.id)" />
-    <p>{{ todo.title }}</p>
-    <IconButton :icon="faXmark" :onClick="onRemove" color="red" />
+    <form v-if="isEditMode" @submit.prevent="onSave" class="todo-container" style="flex: 1">
+      <input type="text" v-model="todoTitle" style="margin-bottom: unset" />
+      <IconButton type="submit" :icon="faFloppyDisk" color="blue" />
+    </form>
+    <p style="margin-bottom: unset; flex: 1" v-else>{{ todo.title }}</p>
+    <div class="todo-container" style="margin-left: 1rem">
+      <IconButton
+        :icon="isEditMode ? faXmark : faPen"
+        :onClick="toggleEdit"
+        :color="isEditMode ? 'brown' : 'blue'"
+      />
+      <IconButton :icon="faTrash" :onClick="onRemove" color="red" />
+    </div>
   </article>
 </template>
 
@@ -24,5 +55,6 @@ function onRemove() {
   display: flex;
   align-items: center;
   justify-content: space-between;
+  gap: 1rem;
 }
 </style>
